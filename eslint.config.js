3f066431795d181876a96b8f0edb0f7bcd8ab7ca
Katelin Frederick <k.frederick94@gmail.com>
2025-03-22 15:06:13 -0400

    Initial Commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,8 @@
 import { FlatCompat } from "@eslint/eslintrc";
 import tseslint from "typescript-eslint";
+import stylistic from '@stylistic/eslint-plugin'
+import perfectionist from 'eslint-plugin-perfectionist'
+import unusedImports from 'eslint-plugin-unused-imports'
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
@@ -17,6 +20,17 @@ export default tseslint.config(
       ...tseslint.configs.recommendedTypeChecked,
       ...tseslint.configs.stylisticTypeChecked,
     ],
+    settings: {
+      'perfectionist': {
+        'type': 'line-length',
+        'partitionByComment': true,
+      },
+    },
+    plugins: {
+      stylistic,
+      perfectionist,
+      unusedImports,
+    },
     rules: {
       "@typescript-eslint/array-type": "off",
       "@typescript-eslint/consistent-type-definitions": "off",
@@ -33,6 +47,116 @@ export default tseslint.config(
         "error",
         { checksVoidReturn: { attributes: false } },
       ],
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          'type': 'line-length',
+          'order': 'desc',
+          'ignoreCase': true,
+          'newlinesBetween': 'always',
+        }
+      ],
+      'perfectionist/sort-interfaces': [
+        'error',
+        {
+          'type': 'line-length',
+          'order': 'desc',
+          'ignoreCase': true,
+        }
+      ],
+      'perfectionist/sort-intersection-types': [
+        'error',
+        {
+          'type': 'line-length',
+          'order': 'desc',
+          'ignoreCase': true,
+        }
+      ],
+      'perfectionist/sort-named-imports': [
+        'error',
+        {
+          'type': 'line-length',
+          'order': 'desc',
+          'ignoreAlias': false,
+          'ignoreCase': true,
+          'groupKind': 'mixed',
+        }
+      ],
+      'unusedImports/no-unused-imports': 'warn',
+      'unusedImports/no-unused-vars': [
+        'warn',
+        {
+          'vars': 'all',
+          'varsIgnorePattern': '^_',
+          'args': 'after-used',
+          'argsIgnorePattern': '^_',
+        }
+      ],
+      'stylistic/semi': [
+        'warn',
+        'never'
+      ],
+      'react-hooks/exhaustive-deps': 'off',
+      'arrow-body-style': [
+        'error',
+        'as-needed'
+      ],
+      'stylistic/quotes': [
+        'error',
+        'single'
+      ],
+      'stylistic/max-len': [
+        2,
+        140,
+        2,
+        {
+          'ignoreUrls': true,
+          'ignoreStrings': true,
+          'ignoreTemplateLiterals': true,
+          'ignoreRegExpLiterals': true,
+          'ignoreComments': true,
+        }
+      ],
+      'no-restricted-syntax': [
+        'error',
+        {
+          'selector': 'FunctionExpression',
+          'message': 'Function expressions are not allowed.',
+        },
+        {
+          'selector': 'FunctionDeclaration',
+          'message': 'Function declarations are not allowed.',
+        }
+      ],
+      'stylistic/comma-dangle': [
+        'warn',
+        {
+          'arrays': 'never',
+          'functions': 'never',
+          'objects': 'always',
+          'imports': 'always',
+          'exports': 'always',
+        }
+      ],
+      'prefer-arrow-callback': 'error',
+      'stylistic/implicit-arrow-linebreak': ['error', 'beside'],
+      'stylistic/indent': ['error', 2],
+      'jsx-a11y/click-events-have-key-events': 'off',
+      'jsx-a11y/no-noninteractive-element-interactions': 'off',
+      'jsx-a11y/no-noninteractive-tabindex': 'off',
+      'stylistic/jsx-quotes': ['error', 'prefer-single'],
+      'stylistic/linebreak-style': 'off',
+      'no-case-declarations': 'error',
+      'no-lonely-if': 'error',
+      'no-nested-ternary': 'error',
+      'no-plusplus': 'off',
+      'no-unused-expressions': 'error',
+      'stylistic/object-curly-newline': ['error', {
+        'ObjectPattern': { 'multiline': true, 'minProperties': 4, },
+        'ObjectExpression': { 'multiline': true, 'minProperties': 4, },
+      }], // TODO: come back and take a look at making objects be in there own line
+      'stylistic/operator-linebreak': ['error', 'before'],
+      'tailwindcss/no-custom-classname': 'off',
     },
   },
   {
